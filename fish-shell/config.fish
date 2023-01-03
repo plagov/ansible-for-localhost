c7196e6dbcbbf27622b4746568dbf02c0e8be7ab
Vitali Plagov <vplagov@pm.me>
2023-01-03 21:47:16 +0200

    Install exa to list files

--- fish-shell/config.fish
+++ fish-shell/config.fish
@@ -1,2 +1,4 @@
 # This file is empty for now because I don't have anything to put in here
 # But I'm sure I'll put some Fish configs in here later on
+
+alias l="exa --long --all --header --no-user"
